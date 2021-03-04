c87be5010bcf2de8c6fad1a01c9ab2065bf213d7
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-03-04 13:24:48 -0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,37 @@
+<?php
+// Our Data
+$title[0] = 'Apple iPhone X';
+$description[0] = 'Apple iPhone X smartphone. Announced Sep 2017. Features 5.8″ Super Retina OLED';
+$price[0] = 1000;
+$photo[0] = 'iphone.jpg';
+
+$title[1] = 'Samsung Galaxy A10';
+$description[1] = 'Samsung Galaxy A10 Android smartphone. Announced Feb 2019. Features 6.2″ IPS LCD display, Exynos 7884 chipset';
+$price[1] = 300;
+$photo[1] = 'samsung.jpg';
+
+$title[2] = 'Xiaomi Mi Note 10';
+$description[2] = 'Xiaomi Mi Note 10 Android smartphone. Announced Nov 2019. Features 6.47″ AMOLED display, Snapdragon';
+$price[2] = 700;
+$photo[2] = 'xiaomi.jpg';
+
+$title[3] = 'Nokia 7.2 Dual Sim';
+$description[3] = 'Nokia 7.2 TA-1196/DS 128GB 6GB RAM (GSM Only, No CDMA) Factory Unlocked No Warranty - 4G LTE International Model';
+$price[3] = 500;
+$photo[3] = 'nokia.jpg';
+
+
+// Pages
+$pages_title['about'] = 'About us';
+$pages_text['about'] = 'I am a PHP programmer and this is an online shop application that I created with PHP and Bootstrap. Im trying to learn more about PHP and make this application even better!';
+
+$pages_title['contact'] = 'Contact us';
+$pages_text['contact'] = '
+<p>You can reach me by email, I check my email every day and get back to you as soon as possible!</p>
+<p>Email: <strong>help[at]mojipo.com</strong></p>
+<p>Also you can find me on twitter! Follow @imojipo</p>';
+
+$pages_title['rules'] = 'Shopping Rules';
+$pages_text['rules'] = 'Here are my rules: <br> 1. Be kind <br> 2. Buy something 3. <br> 3. No refunds ';
+
+?>
